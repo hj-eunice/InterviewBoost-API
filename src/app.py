edc0792db55367a93c94b4ca3f1d2df9591f05d1
Hyunji Kim <hj.eunice@gmail.com>
2023-10-20 16:46:15 -0700

    POST /transcribe

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
+# system packages
+import base64
+
 # third-party packages
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # local packages
 from utils.cache import get_cached_starting_questions
+from utils.speech_to_text import speech_to_text
 
 app = Flask(__name__)
 CORS(app)
@@ -22,6 +26,25 @@ def generate_first_question():
         "question": question,
         "job_title": job_title,
         "job_level": job_level
+    })
+
+
+@app.route("/transcribe", methods=["POST"])
+def transcribe():
+    transcript = ""
+    try:
+        req_form = request.form
+        decoded_data = base64.b64decode(req_form['fileBase64'])
+        with open('/tmp/audio.webm', 'wb') as f:
+            f.write(decoded_data)
+        audio_file = open('/tmp/audio.webm', "rb")
+        transcript = speech_to_text(audio_file)
+    except Exception as e:
+        print(e)
+
+    return jsonify({
+        "success": True,
+        "transcript": transcript
     })
 
 if __name__ == '__main__':
